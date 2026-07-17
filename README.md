8587a2a4dfa1ce80468b07184c8e18a6c38aa50f
juanpablomgr <juanp.mg12@gmail.com>
2026-07-17 17:16:34 -0500

    fic: readme

--- README.md
+++ README.md
@@ -1,8 +1,44 @@
 # Proyecto SQL-Server-Commercial-Analytics
 Proyecto Integral de SQL Server para Analitica de Negocios
 ## Resumen (overview)
-_Velmora Global Trading Co. es una empresa de distribución y venta retail de multicategoría. Opera bajo un modelo híbrido B2C-B2B atendiendo a pequeñas oficinas y a grandes corporaciones._
+_Corwell Group es una empresa de distribución y venta retail de multicategoría con operaciones y presencia en Estados Unidos._
 
-_El departamento de ventas y operaciones de Velmora Global desea mejorar la rentabilidad, optimizar la operación logística y entender el comportamiento de compra de sus clientes en los mercados donde opera. Sin embargo, no cuenta con una visión clara de sus datos transaccionales._
+_El departamento de estrategia comercial de Corwell Group desea identificar en qué productos, categorías y mercados regionales debe enfocar sus esfuerzos de crecimiento y rentabilidad, priorizando decisiones de portafolio sobre una base de datos sólida en lugar de intuición._
 
-_El objetivo del proyecto es utilizar el lenguaje **SQL** dentro del entorno de **SQL Server Management Studio**_ para estructurar, normalizar y analizar la información transaccional del negocio con el fin de entregar al área comercial y operaciones recomendaciones accionables que permitan implementar mejoras practicas en su estrategia comercial, optimizar sus procesos y aumentar la rentabilidad._
+_El objetivo del proyecto es utilizar el lenguaje **SQL** dentro del entorno de **SQL Server Management Studio**_ para estructurar, normalizar y analizar la información transaccional del negocio con el fin de entregar al área comercial recomendaciones accionables que permitan implementar mejoras practicas en su estrategia comercial, optimizar sus procesos y aumentar la rentabilidad._
+
+## Estructura del Proyecto
+- [Sobre los Datos](#sobre-los-datos)
+- [Tareas](#tareas)
+- [Arquitectura y Modelado de Datos](#arquitectura-y-modelado-de-datos)
+- [Limpieza de Datos](#limpieza-de-datos)
+- [Análisis Exploratorio de Datos e Insights](#análisis-exploratorio-de-datos-e-insights)
+- [Conclusiones y Recomendaciones](#conclusiones-y-recomendaciones)
+
+## Sobre los datos
+
+Este proyecto utiliza datos provenientes de un único archivo .csv el cual contiene información sobre las transacciones de ventas de una empresa retail.
+
+El dataset está compuesto por 200,000 registros, se puede acceder al dataset original [aquí](https://www.kaggle.com/datasets/yashyennewar/product-sales-dataset-2023-2024).
+
+A continuación se muestra la estructura de las tablas del dataset:
+
+### Estructura del archivo `sales.csv`
+
+| Columna | Descripción |
+|----------|-------------|
+| Order_ID | Identificador único de cada pedido. |
+| Order_Date | Fecha en la que se realizó la transacción. |
+| Customer_Name | Nombre del cliente que realizó la compra. |
+| City | Ciudad de residencia del cliente. |
+| State | Estado de residencia del cliente. |
+| Region | Región geográfica donde se ubica el cliente (Este, Oeste, Sur o Centro). |
+| Country | País donde se realizó la venta (Estados Unidos). |
+| Category | Categoría principal del producto (por ejemplo, Accesorios, Ropa y Vestimenta). |
+| Sub_Category | Subcategoría del producto dentro de la categoría principal (por ejemplo, Ropa deportiva, Bolsos). |
+| Product_Name | Nombre o descripción del producto comercializado. |
+| Quantity | Cantidad de unidades compradas. |
+| Unit_Price | Precio unitario del producto, expresado en dólares estadounidenses (USD). |
+| Revenue | Ingresos totales generados por la venta, calculados como **Cantidad × Precio Unitario**. |
+| Profit | Ganancia neta obtenida en la transacción. |
+
--- Scripts/dql.sql
+++ Scripts/dql.sql
@@ -20,7 +20,7 @@ WHERE g.Geo_ID IS NULL;
 -- Valores Nulos o Faltantes
 ------------------------------
 
--- Verificación de valores faltantes en la tabla Dim_Customer
+-- Verificacion de valores faltantes en la tabla Dim_Customer
 
 SELECT COUNT(1) as ValoresFaltantesCustomer
 FROM Dim_Customer
@@ -34,7 +34,7 @@ WHERE Product_Name is NULL
 		or Category is NULL
 		or Sub_Category is NULL;
 
--- Verificación de valores faltantes en la tabla Dim_Geography
+-- Verificacion de valores faltantes en la tabla Dim_Geography
 
 SELECT COUNT(1) as ValoresFaltantesGeo
 FROM Dim_Geography
@@ -43,7 +43,7 @@ WHERE City is NULL
 		or Region IS NULL
 		or Country IS NULL;
 
--- Verificación de valores faltantes en la tabla Fact_Sales
+-- Verificacion de valores faltantes en la tabla Fact_Sales
 
 SELECT COUNT(1) as ValoresFaltantesFact
 FROM Fact_Sales
@@ -60,7 +60,7 @@ WHERE  Order_ID IS NULL
 ------------------------
 -- Valores Duplicados
 ------------------------
--- Verificación de valores duplicados en la tabla Fact_Sales
+-- Verificacion de valores duplicados en la tabla Fact_Sales
 
 SELECT Order_ID, COUNT(1) as ValoresDuplicados
 FROM Fact_Sales
@@ -69,16 +69,16 @@ HAVING COUNT(1) > 1;
 
 
 -------------------------------------
--- Verificación de Inconsistencias
+-- Verificacion de Inconsistencias
 -------------------------------------
 
--- Verificación de Formulas en Campos Calculados de Revenue
+-- Verificacion de Formulas en Campos Calculados de Revenue
 
 SELECT COUNT(1) AS CalculoIncorrecto
 FROM sales
 WHERE ROUND(Unit_Price * Quantity, 2) <> Revenue;
 
--- Verificación de inconsistencia (se debe cumplir Profit < Revenue)
+-- Verificacion de inconsistencia (se debe cumplir Profit < Revenue)
 
 SELECT COUNT(1) AS Inconsistencia_Profit_Revenue
 FROM sales
@@ -89,18 +89,126 @@ WHERE Profit > Revenue;
 -- PREGUNTAS DE NEGOCIO ---------------------------------------------
 --------------------------------------------------------------------
 
---- Revenue y Profit por Categoría de Producto
--- ¿ Qué categorías de producto son las que generan más ingresos y ganancia ?
-
+--- Pregunta 1: Ingreso y Margen Total por Categoria de Producto
+-- ¿Cual es el ingreso total y la ganancia total generados por cada categoría de producto, y que porcentaje de margen representa cada una?
 SELECT 
 	p.Category,
-	p.Sub_Category,
-	sum(s.Revenue) as RevenueTotal,
-	sum(s.Profit) as ProfitTotal
-FROM Fact_Sales as s
-JOIN Dim_Product as p on p.Product_ID  = s.Product_ID
-group by p.Category, p.Sub_Category
+	FORMAT(SUM(f.Revenue), 'N2') AS  IngresoTotal,  -- Formato de Separación
+	FORMAT(SUM(f.Profit), 'N2') AS MargenTotal,		-- Formato de Separación
+	CAST(	
+		(SUM(f.Profit) * 100.0)/ SUM(f.Revenue) AS DECIMAL(10,2) 
+		) AS [PctMargen (%)]
+FROM Fact_Sales AS f
+INNER JOIN Dim_Product AS p ON p.Product_ID  = f.Product_ID
+GROUP BY p.Category
 ORDER BY 
-	sum(s.Revenue) DESC,
-	sum(s.Profit) DESC
+	[PctMargen (%)] DESC;
 
+--- Pregunta 2: Mejores Productos del Catalogo por Margen
+-- ¿Cuales son los productos que generan mayor ingreso para la empresa?
+
+SELECT TOP 10
+	p.Product_Name,
+	FORMAT(SUM(f.Revenue), 'N2') AS  IngresoTotal,
+	FORMAT(SUM(f.Profit), 'N2') AS MargenTotal,
+	CAST(	
+		(SUM(f.Profit) * 100.0)/ SUM(f.Revenue) AS DECIMAL(10,2) 
+		) AS [PctMargen (%)]
+FROM Fact_Sales AS f
+JOIN Dim_Product as p ON p.Product_ID = f.Product_ID
+GROUP BY p.Product_Name
+ORDER BY SUM(f.Revenue) DESC --- Ordenamos por 'SUM(f.Revenue)' para incluir los valores numéricos en el ordenamiento
+							 --- y no los textuales puesto que se utilizó 'FORMAT'
+
+--- Pregunta 3: Ventas Totales Por Region
+-- ¿Como se comparan las ventas totales, la ganancia y la cantidad de de órdenes entre las 4 regiones donde opera el negocio?
+
+SELECT 
+	g.Region,
+	FORMAT(SUM(f.Revenue), 'N2') AS  IngresoTotal,
+	FORMAT(SUM(f.Profit), 'N2') AS MargenTotal,
+	CAST(	
+		(SUM(f.Profit) * 100.0)/ SUM(f.Revenue) AS DECIMAL(10,2) 
+		) AS [PctMargen (%)],
+	COUNT(DISTINCT f.Order_ID) AS CntOrdenes
+FROM Fact_Sales AS f
+INNER JOIN Dim_Geography AS g ON g.Geo_ID = f.Geo_ID
+GROUP BY g.Region
+ORDER BY SUM(f.Revenue) DESC
+
+-- Pregunta 4: Rentabilidad de los productos
+-- ¿Como se puede clasificar a cada producto según su nivel de rentabilidad (alta, media o baja) para priorizar decisiones comerciales?
+
+SELECT 
+	p.Product_Name,
+	FORMAT(SUM(f.Revenue), 'N2') AS  IngresoTotal,
+	FORMAT(SUM(f.Profit), 'N2') AS MargenTotal,
+	CAST(	
+		(SUM(f.Profit) * 100.0)/ SUM(f.Revenue) AS DECIMAL(10,2) 
+		) AS [PctMargen (%)],
+	CASE 
+		WHEN (SUM(f.Profit) * 100.0)/ SUM(f.Revenue) >= 33 THEN 'Alto'
+		WHEN (SUM(f.Profit) * 100.0)/ SUM(f.Revenue) >= 17 THEN 'Medio'
+		ELSE 'Bajo'
+	END AS ClasificacionRentabilidad
+FROM Fact_Sales  AS f
+INNER JOIN Dim_Product AS p ON p.Product_ID = f.Product_ID
+GROUP BY p.Product_Name
+ORDER BY [PctMargen (%)] DESC, SUM(f.Profit) DESC, SUM(f.Revenue) DESC
+
+-- Pregunta 5: Evolución Mensual de Ingresos
+-- ¿Como ha evolucionado el ingreso mes a mes durante 2023 y 2024? ¿Existe algun patron de estacionalidad?
+
+SELECT 
+    YEAR(f.Order_Date) AS Anio,
+    MONTH(f.Order_Date) AS NumMes,
+    DATENAME(MONTH, f.Order_Date) AS Mes,
+    FORMAT(SUM(f.Revenue), 'N2') AS IngresoTotal,
+    FORMAT(SUM(f.Profit), 'N2') AS MargenTotal,
+    COUNT(DISTINCT f.Order_ID) AS TotalOrdenes
+FROM Fact_Sales AS f
+GROUP BY YEAR(f.Order_Date), MONTH(f.Order_Date), DATENAME(MONTH, f.Order_Date)
+ORDER BY Anio ASC, NumMes ASC;
+
+-- Pregunta 6: Estados que concentran el 80% del ingreso total (análisis Pareto)
+
+-- ¿Qué estados concentran el 80% del ingreso total de la empresa? ¿Vale la pena distribuir el esfuerzo comercial por igual entre los 47 estados?
+
+
+WITH IngresoPorEstado AS (
+    SELECT 
+        g.State,
+        SUM(f.Revenue) AS IngresoEstado
+    FROM Fact_Sales AS f
+    JOIN Dim_Geography AS g ON g.Geo_ID = f.Geo_ID
+    GROUP BY g.State
+),
+Acumulado AS (
+    SELECT 
+        State,
+        IngresoEstado,
+        SUM(IngresoEstado) OVER (ORDER BY IngresoEstado DESC) AS IngresoAcumulado,
+        SUM(IngresoEstado) OVER () AS IngresoTotalGeneral
+    FROM IngresoPorEstado
+)
+SELECT 
+    State,
+    FORMAT(IngresoEstado, 'N2') AS Ingreso_por_Estado,
+    CAST((IngresoEstado * 100.0 / IngresoTotalGeneral) AS DECIMAL(10,2)) AS PctIndividual,
+    CAST((IngresoAcumulado * 100.0 / IngresoTotalGeneral) AS DECIMAL(10,2)) AS PctAcumulado
+FROM Acumulado
+ORDER BY IngresoEstado DESC;
+
+
+
+
+/* 
+¿Qué proporción de clientes ha realizado más de una compra, frente a los que solo compraron una vez?
+
+¿Cuál es la tasa de crecimiento mes a mes del ingreso por categoría de producto, y qué categorías muestran una tendencia sostenida de crecimiento o caída?
+¿Cuáles son los productos más y menos rentables dentro de cada categoría, ordenados por ranking interno?
+¿Existen productos con una caída sostenida de ventas durante 2 o más meses consecutivos que ameriten una alerta temprana?
+¿Cómo se segmentan los clientes según su valor de compra acumulado (en cuartiles), y qué porcentaje del ingreso total aporta el cuartil superior?
+¿Cuál es la contribución acumulada de cada región al margen de ganancia total de la empresa?
+¿Qué combinaciones de categoría y región presentan el mejor y el peor desempeño conjunto de ingreso y margen?
+¿Existen productos con un precio unitario anormalmente alto o bajo respecto al promedio de su subcategoría, que ameriten revisión de pricing? */
